baaf36c646ecc6b525cdcd6aaff345725ce144f6
findasifrahman <findasifrahman@gmail.com>
2025-04-24 04:44:43 +0600

    fixed db connection error4

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const prisma = global.prisma || new PrismaClient({
+const prismaClient = globalForPrisma.prisma ?? new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-})
+});
 
 if (process.env.NODE_ENV !== 'production') {
-  global.prisma = prisma
+  globalForPrisma.prisma = prismaClient;
 }
 
-export { prisma } 
+export const prisma = prismaClient; 
