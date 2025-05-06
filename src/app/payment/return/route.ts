f2ed36ef525cbeb71845f823c331e1f63dc99769
findasifrahman <findasifrahman@gmail.com>
2025-05-07 02:40:30 +0600

    hello3

--- src/app/payment/return/route.ts
+++ src/app/payment/return/route.ts
@@ -86,7 +86,7 @@ export async function GET(req: Request) {
         // Then create the esimOrderAfterPayment record
         const orderAfterPayment = await prisma.esimOrderAfterPayment.create({
           data: {
-            orderId: orderId,
+            orderNo: orderId,
             paymentOrderNo: orderId,
             orderNo:"PRPCESSING-" + orderId,
             userId: session.user.id,
